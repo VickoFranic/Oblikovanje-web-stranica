fc9e3403bae43b02066446d8f76a31d5fd241de6
VickoFranic <vfranic@gmail.com>
2015-04-27 11:44:54 +0200

    ColorBox plugin dodan

--- OWS_Seminar/Workspace/js/retes.js
+++ OWS_Seminar/Workspace/js/retes.js
@@ -5,7 +5,11 @@ $(document).ready(function() {
 		target: ".navbar-fixed-top"
 	})
 
+// Intro naslov fadeIn i slideDown na strelicu
 	$(".intro-naslov").fadeIn(1500, function() {
 		$(".strelica").slideDown("slow");
 	});
+
+	// ColorBox na slike u print section
+	$(".work").colorbox({fixed:true});
 });
